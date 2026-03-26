689938ffb5c7e507e8ee3e602c682e09a49135eb
DevPastey <emmanuelolabodee@outlook.com>
2026-03-26 16:35:00 +0100

    UI update for  loginPage

--- dims-frontend/components/ui/Input.tsx
+++ dims-frontend/components/ui/Input.tsx
@@ -9,15 +9,18 @@ import { UseFormRegisterReturn } from "react-hook-form";
 interface InputProp {
   title: string;
   placeholder: string;
-  register: UseFormRegisterReturn
+  register: UseFormRegisterReturn;
+  type: string;
+  name: string;
+  autoComplete: string
 }
 
-export default function Input({title, placeholder, register}: InputProp) {
+export default function Input({title, placeholder, register, type, autoComplete, name}: InputProp) {
   // TODO: Implement
   return (
     <div >
       <p className="text-xs mb-1 text-gray-500">{title}</p>
-      <input {...register} type="text"  className="border-gray-300 rounded pl-3 focus:outline-none focus:ring-2 focus:ring-dana-blue-300 focus:border-transparent border-[1px] py-1 shadow-sm w-full target:text-gray-400 placeholder:text-xs"  placeholder={placeholder}/>
+      <input {...register} type={type} name={name} autoComplete={autoComplete} className="border-gray-300 rounded pl-3 focus:outline-none focus:ring-2 focus:ring-dana-blue-300 focus:border-transparent border-[1px] py-2 shadow-sm w-full target:text-gray-400 placeholder:text-xs"  placeholder={placeholder}/>
     </div>
   )
 }
